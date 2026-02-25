71ec08b2ee503aa229a4f89b5b342ee13f728986
Ramit Roshann <ramitroshan777777@gmail.com>
2026-02-25 13:10:56 +0530

    feat: Implement GET, PATCH, Post and Delete routes

--- backend/index.js
+++ backend/index.js
@@ -5,21 +5,75 @@ const User = require("./src/models/user");
 const port = 3000;
 require("dotenv").config();
 
+//middleware
+app.use(express.json());
+
 app.post("/signup", async (req, res) => {
   //creating a new instance of the User model
-  const user = new User({
-    firstName: "Ramit",
-    lastName: "Roshan",
-    emailId: "ramit@roshangmail.com",
-    password: "ramit@123",
-  });
+  const user = new User(req.body);
 
-  //instance of model and this .save() will save data in the database model
+  //instance of model and using user.save(), user save data in the database model
   try {
     await user.save();
     res.send("User Added successfully!");
-  }catch(err){
+  } catch (err) {
     res.status(400).send("Error saving the user:" + err.message);
+  }
+});
+
+//Get user by email
+app.get("/user", async (req, res) => {
+  const userEmail = req.body.emailId;
+
+  try {
+    const users = await User.findOne({ emailId: userEmail });
+    //when we didn't found user
+    if (!users) {
+      res.status(404).send("User not found");
+    } else {
+      res.send(users);
+    }
+  } catch (err) {
+    res.status(400).send("Something went wrong");
+  }
+});
+
+// Feed API - get/print all the users form the database
+app.get("/feed", async (req, res) => {
+  try {
+    const users = await User.find({});
+    res.send(users);
+  } catch (err) {
+    res.status(400).send("Something went wrong");
+  }
+});
+
+//delete user API - deleting a user by its id
+app.delete("/user", async (req, res) => {
+  const userId = req.body.userId; //read data
+
+  try {
+    //const user = await User.findByIdAndDelete({_id: userId}); //useId is not in models so we can use like this also
+    const user = await User.findByIdAndDelete(userId);
+    res.send("User deleted succesfully!");
+  } catch (err) {
+    res.status(400).send("something went wrong!!");
+  }
+});
+
+// patch/update user API - updating the data of user
+app.patch("/user", async (req, res) => {
+  const userId = req.body.userId; //find and read userID
+  const data = req.body; //read the whole data
+
+  try {
+    const user = await User.findByIdAndUpdate({ _id: userId }, data, {
+      returnDocument: "before",
+    }); //return the documents before updates
+    console.log(user);
+    res.send("User updated successfully");
+  } catch (err) {
+    res.status(400).send("something went wrong!!");
   }
 });
 
