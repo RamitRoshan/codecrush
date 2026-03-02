76e1f2b4bd43d49f4026c19fff31c05dbe4dec37
Ramit Roshann <ramitroshan777777@gmail.com>
2026-03-02 21:34:27 +0530

    feat: implements .pre so that it checks before saving same user is send request to themselves

--- backend/src/models/connectionRequest.js
+++ backend/src/models/connectionRequest.js
@@ -1,28 +1,40 @@
 const mongoose = require("mongoose");
 
 //Connection request schema
-const connectionRequestSchema = new mongoose.Schema({
-
-
+const connectionRequestSchema = new mongoose.Schema(
+  {
     fromUserId: {
-        type: mongoose.Schema.Types.ObjectId,
-        required: true,
+      type: mongoose.Schema.Types.ObjectId,
+      required: true,
     },
     toUserId: {
-        type: mongoose.Schema.Types.ObjectId,
-        required: true,
+      type: mongoose.Schema.Types.ObjectId,
+      required: true,
     },
     status: {
-        type: String,
-        required: true,
-        enum: {
-            values: ["ignored", "interested", "accepted", "rejected"],
-            message: `{values} is incorrect status type`, //throw error, when anything else
-        }
-    }
+      type: String,
+      required: true,
+      enum: {
+        values: ["ignored", "interested", "accepted", "rejected"],
+        message: `{values} is incorrect status type`, //throw error, when anything else
+      },
+    },
+  },
+  { timestamps: true },
+);
 
-}, {timestamps: true}) 
+connectionRequestSchema.pre("save", function (next) {
+  const connectionRequest = this;
+  //before saving,it will check if the fromUserId is same as toUserId
+  if (connectionRequest.fromUserId.equals(connectionRequest.toUserId)) {
+    throw new Error("You Cannot send connection request to yourself!");
+  }
+  //this is kind of middleware so call next();
+  next();
+});
 
-
-const ConnectionRequestModel = new mongoose.model("ConnectionRequestModel", connectionRequestSchema);
+const ConnectionRequestModel = new mongoose.model(
+  "ConnectionRequestModel",
+  connectionRequestSchema,
+);
 module.exports = ConnectionRequestModel;
